4649baf54e975c37e54377e544988d88950caaa7
tomieq <tomieq@wp.pl>
2025-04-25 16:34:34 +0200

    Added Array chunked and cut functions

--- Sources/SwiftExtensions/Array+extension.swift
+++ Sources/SwiftExtensions/Array+extension.swift
@@ -5,6 +5,10 @@
 //  Created by Tomasz on 24/01/2025.
 //
 
+public enum ArrayError: Error {
+    case cutError(String)
+}
+
 extension Array {
     public subscript(safeIndex index: Int) -> Element? {
         get {
@@ -55,5 +59,21 @@ extension Array {
 
     public func subArray(_ range: PartialRangeUpTo<Int>) -> [Element] {
         self[range].array
+    }
+}
+
+extension Array {
+    public func chunked(by chunkSize: Int) -> [[Element]] {
+        return stride(from: 0, to: self.count, by: chunkSize).map {
+            self[$0..<Swift.min($0 + chunkSize, self.count)].array
+        }
+    }
+
+    // cuts array into equal subarrays
+    public func cut(into parts: Int) throws -> [[Element]] {
+        guard self.count % parts == 0 else {
+            throw ArrayError.cutError("Cannot cut array into \(parts) subarrays as the array length is not dividable by \(parts)")
+        }
+        return self.chunked(by: self.count / parts)
     }
 }
