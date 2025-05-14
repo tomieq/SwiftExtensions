4dc97319a3efadf9fa5b3c69d709a0ccfbfc9f8b
tomieq <tomieq@wp.pl>
2025-05-14 16:03:37 +0200

    Fixed working with Data slices

--- Sources/SwiftExtensions/Data+numbers.swift
+++ Sources/SwiftExtensions/Data+numbers.swift
@@ -9,15 +9,15 @@ import Foundation
 extension Data {
     // big endian
     public var uInt8: UInt8 {
-        UInt8(self[0])
+        UInt8(self[startIndex])
     }
     // big endian
     public var uInt16: UInt16 {
-        UInt16(self[0]) << 8 + UInt16(self[1])
+        UInt16(self[startIndex]) << 8 + UInt16(self[startIndex + 1])
     }
 
     // big endian
     public var uInt32: UInt32 {
-        UInt32(self[0]) << 24 + UInt32(self[1]) << 16 + UInt32(self[2]) << 8 + UInt32(self[3])
+        UInt32(self[startIndex]) << 24 + UInt32(self[startIndex + 1]) << 16 + UInt32(self[startIndex + 2]) << 8 + UInt32(self[startIndex + 3])
     }
 }
