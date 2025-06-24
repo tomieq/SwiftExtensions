3b7acddacc09bb34c67c37ead94fd364108d1c82
tomieq <tomieq@wp.pl>
2025-06-24 15:01:42 +0200

    Implemented Int to Data converter

--- Sources/SwiftExtensions/data/Data+numbers.swift
+++ Sources/SwiftExtensions/data/Data+numbers.swift
@@ -56,23 +56,21 @@ extension Data {
     public var int: Int {
         get throws {
             guard self.isEmpty.not else {
-                throw DataToNumberConvertError.notEnoughNumberOfBytes
-            }
-            let bytes = self.bytes
-            switch count {
-            case 1:
-                return Int(bytes[0])
-            case 2:
-                return Int(bytes[0]) << 8 + Int(bytes[1])
-            case 3:
-                return Int(bytes[0]) << 16 + Int(bytes[1]) << 8 + Int(bytes[2])
-            case 4:
-                let high = Int(bytes[0]) << 24 + Int(bytes[1]) << 16
-                let low = Int(bytes[2]) << 8 + Int(bytes[3])
-                return high + low
-            default:
-                throw DataToNumberConvertError.tooManyOfBytes
-            }
+                 throw DataToNumberConvertError.notEnoughNumberOfBytes
+             }
+             if count > 8 {
+                 throw DataToNumberConvertError.tooManyOfBytes
+             }
+
+             var value: UInt64 = 0
+             for byte in self {
+                 value = (value << 8) | UInt64(byte)
+             }
+             if value > UInt64(Int.max) {
+                 throw DataToNumberConvertError.tooManyOfBytes
+             }
+
+             return Int(value)
         }
     }
 }
--- Sources/SwiftExtensions/numeric/Int+extension.swift
+++ Sources/SwiftExtensions/numeric/Int+extension.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Tomasz on 24/01/2025.
 //
+import Foundation
 
 extension Int {
     public var incremented: Int {
@@ -28,5 +29,17 @@ extension Int {
 public extension Int {
     var isMax: Bool {
         self == Int.max
+    }
+}
+
+public extension Int {
+    /// converts Int do Data (big-endian), with minimal byte number
+    var data: Data {
+        var bytes: [UInt8] = []
+        repeat {
+            let byte = UInt8(truncatingIfNeeded: self >> ((bytes.count) * 8))
+            bytes.insert(byte, at: 0)
+        } while (self >> (bytes.count * 8)) != 0
+        return Data(bytes)
     }
 }
