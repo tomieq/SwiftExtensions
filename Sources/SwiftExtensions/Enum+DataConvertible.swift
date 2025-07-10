a8d8a30ec760a10f9f9e93cf3e291d38001657c0
tomieq <tomieq@wp.pl>
2025-07-10 11:53:41 +0200

    Added .data to RawRepresentable

--- Sources/SwiftExtensions/Enum+DataConvertible.swift
+++ Sources/SwiftExtensions/Enum+DataConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  Enum+compare.swift
+//  Enum+DataConvertible.swift
 //  SwiftExtensions
 //
 //  Created by Tomasz Kucharski on 10/07/2025.
@@ -12,5 +12,11 @@ public extension RawRepresentable where RawValue: DataConvertible {
     }
     static func == (lhs: DataConvertible, rhs: Self) -> Bool {
         lhs.data == rhs.rawValue.data
+    }
+}
+
+extension RawRepresentable where RawValue: DataConvertible {
+    public var data: Data {
+        rawValue.data
     }
 }
